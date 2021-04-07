9105b0757f609dca7b1c3371787d28ab53ee9e80
Primož Kerin <kerin.primoz@gmail.com>
2021-04-07 18:40:50 +0200

    Fix center positioning

--- pages/blog-post-bread.vue
+++ pages/blog-post-bread.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="flex flex-col pb-8">
+    <div class="flex flex-col items-center pb-8">
       <nuxt-link
         to="/"
         class="block text-green-500 border-solid border-b-2 py-2 px-2 border-green-500 mt-8 w-16 self-center"
